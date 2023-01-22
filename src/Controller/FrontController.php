6562e68db21b3e6681365f05c3203693acc020bc
Yulia Dubenchuk <yulia.dubenchuk@ab-soft.net>
2023-01-22 20:02:29 +0200

    Lesson 117 | Make template links part 2

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -31,4 +31,36 @@ class FrontController extends AbstractController
     {
         return $this->render('front/playlist_details.html.twig');
     }
+
+    /**
+     * @Route("/search-results", methods={"POST"}, name="search_results")
+     */
+    public function searchResults(): Response
+    {
+        return $this->render('front/search_results.html.twig');
+    }
+
+    /**
+     * @Route("/pricing", name="pricing")
+     */
+    public function pricing(): Response
+    {
+        return $this->render('front/pricing.html.twig');
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(): Response
+    {
+        return $this->render('front/register.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(): Response
+    {
+        return $this->render('front/login.html.twig');
+    }
 }
