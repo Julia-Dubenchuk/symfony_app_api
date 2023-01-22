c4a8e96481d8f3856589121a44508fbb9fb0a4d9
Yulia Dubenchuk <yulia.dubenchuk@ab-soft.net>
2023-01-22 15:30:36 +0200

    Lesson 115 | Add assets files

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class FrontController extends AbstractController
 {
     /**
-     * @Route("/front", name="app_front")
+     * @Route("/", name="main_page")
      */
     public function index(): Response
     {
