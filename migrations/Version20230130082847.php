54cb691a1a8ea2c1b2c7dba6f88572613f3afe3a
Yulia Dubenchuk <yulia.dubenchuk@ab-soft.net>
2023-01-30 10:30:47 +0200

    Lectures 123 - 124 | Category fixtures

--- migrations/Version20230130082847.php
+++ migrations/Version20230130082847.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230130070519 extends AbstractMigration
+final class Version20230130082847 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -11,6 +11,12 @@ class CategoryFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $this->loadMainCategories($manager);
+        $this->loadChill($manager);
+        $this->loadPop($manager);
+        $this->loadFreshPop($manager);
+        $this->loadNoStress($manager);
+        $this->loadParty($manager);
+        $this->loadWorkout($manager);
     }
 
     private function loadMainCategories(ObjectManager $manager)
@@ -26,6 +32,53 @@ class CategoryFixtures extends Fixture
         $manager->flush();
     }
 
+    private function loadSubcategories(ObjectManager $manager, $category, $parent_id)
+    {
+        $parent = $manager->getRepository(Category::class)->find($parent_id);
+        $methodName = "get{$category}Data";
+
+        foreach($this->$methodName() as [$name])
+        {
+            $category = new Category();
+            $category->setName($name);
+            $category->setParent($parent);
+            $manager->persist($category);
+        }
+    
+
+        $manager->flush();
+    }
+
+    private function loadChill(ObjectManager $manager)
+    {
+        $this->loadSubcategories($manager, 'Chill', 1);
+    }
+
+    private function loadPop(ObjectManager $manager)
+    {
+        $this->loadSubcategories($manager, 'Pop', 6);
+    }
+
+    private function loadFreshPop(ObjectManager $manager)
+    {
+        $this->loadSubcategories($manager, 'FreshPop', 8);
+    }
+
+    private function loadWorkout(ObjectManager $manager)
+    {
+        $this->loadSubcategories($manager, 'Workout', 3);
+    }
+
+    private function loadParty(ObjectManager $manager)
+    {
+        $this->loadSubcategories($manager, 'Party', 4);
+    }
+
+    private function loadNoStress(ObjectManager $manager)
+    {
+        $this->loadSubcategories($manager, 'NoStress', 18);
+    }
+
     private function getMainCategoriesData()
     {
         return [
@@ -33,6 +86,58 @@ class CategoryFixtures extends Fixture
             ['Lofi', 2],
             ['Workout', 3],
             ['Party', 4],
+        ];
+    }
+
+    private function getChillData()
+    {
+        return [
+            ['Anime', 5],
+            ['Pop', 6],
+            ['Cozy coffeshop', 7],
+        ];
+    }
+
+    private function getPopData()
+    {
+        return [
+            ['Fresh Pop', 8],
+            ['Global Hits', 9],
+        ];
+    }
+
+    private function getFreshPopData()
+    {
+        return [
+            ['Feel Good', 10],
+            ['Feeling Blue', 11],
+            ['Acoustic', 12],
+            ['At home', 13],
+            ['Focus', 14],
+        ];
+    }
+
+    private function getWorkoutData()
+    {
+        return [
+            ['Rock', 15],
+            ['Metal', 16],
+        ];
+    }
+
+    private function getPartyData()
+    {
+        return [
+            ['Lowkey', 17],
+            ['No Stress', 18],
+        ];
+    }
+
+    private function getNoStressData()
+    {
+        return [
+            ['Young and Free', 19],
+            ['Happy Hour', 20],
         ];
     }
 }
