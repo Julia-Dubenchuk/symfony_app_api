9bec2212724e1e85d6bc9bd4690492f2dfb06deb
Yulia Dubenchuk <yulia.dubenchuk@ab-soft.net>
2023-02-13 20:59:40 +0200

    Security

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -25,7 +25,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/categories", name="categories", methods={"GET", "POST"})
+     * @Route("/su/categories", name="categories", methods={"GET", "POST"})
      */
     public function categories(CategoryTreeAdminList $categories, Request $request): Response
     {
@@ -68,7 +68,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/users", name="users")
+     * @Route("/su/users", name="users")
      */
     public function users(): Response
     {
@@ -76,7 +76,7 @@ class AdminController extends AbstractController
     }
 
      /**
-     * @Route("/edit-category/{id}", name="edit_category", methods={"GET", "POST"})
+     * @Route("/su/edit-category/{id}", name="edit_category", methods={"GET", "POST"})
      */
     public function editCategory(Category $category, Request $request): Response
     {
@@ -100,7 +100,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/delete-category/{id}", name="delete_category")
+     * @Route("/su/delete-category/{id}", name="delete_category")
      */
     public function deleteCategory(Category $category): Response
     {
@@ -113,6 +113,8 @@ class AdminController extends AbstractController
 
     public function getAllCategories(CategoryTreeAdminOptionList $categories, $editedCategory = null)
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        
         $categories->getCategoryList($categories->buildTree());
 
         return $this->render('admin/_all_categories.html.twig',[
